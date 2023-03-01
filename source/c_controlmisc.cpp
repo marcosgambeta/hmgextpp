7bf10924b18a729d83c5e213b46f2923594d6828
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2023-03-01 20:15:44 -0300

    Update c_controlmisc.cpp

--- source/c_controlmisc.cpp
+++ source/c_controlmisc.cpp
@@ -1,61 +1,56 @@
-/*----------------------------------------------------------------------------
-   MINIGUI - Harbour Win32 GUI library source code
-
-   Copyright 2002-2010 Roberto Lopez <harbourminigui@gmail.com>
-   http://harbourminigui.googlepages.com/
-
-   This program is free software; you can redistribute it and/or modify it under
-   the terms of the GNU General Public License as published by the Free Software
-   Foundation; either version 2 of the License, or (at your option) any later
-   version.
-
-   This program is distributed in the hope that it will be useful, but WITHOUT
-   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License along with
-   this software; see the file COPYING. If not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA (or
-   visit the web site http://www.gnu.org/).
-
-   As a special exception, you have permission for additional uses of the text
-   contained in this release of Harbour Minigui.
-
-   The exception is that, if you link the Harbour Minigui library with other
-   files to produce an executable, this does not by itself cause the resulting
-   executable to be covered by the GNU General Public License.
-   Your use of that executable is in no way restricted on account of linking the
-   Harbour-Minigui library code into it.
-
-   Parts of this project are based upon:
-
-    "Harbour GUI framework for Win32"
-    Copyright 2001 Alexander S.Kresin <alex@kresin.ru>
-    Copyright 2001 Antonio Linares <alinares@fivetech.com>
-    www - https://harbour.github.io/
-
-    "Harbour Project"
-    Copyright 1999-2022, https://harbour.github.io/
-
-    "WHAT32"
-    Copyright 2002 AJ Wos <andrwos@aust1.net>
-
-    "HWGUI"
-    Copyright 2001-2021 Alexander S.Kresin <alex@kresin.ru>
-
-   ---------------------------------------------------------------------------*/
+/*
+ * MINIGUI - Harbour Win32 GUI library source code
+ *
+ * Copyright 2002-2010 Roberto Lopez <harbourminigui@gmail.com>
+ * http://harbourminigui.googlepages.com/
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this software; see the file COPYING. If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA (or
+ * visit the web site http://www.gnu.org/).
+ *
+ * As a special exception, you have permission for additional uses of the text
+ * contained in this release of Harbour Minigui.
+ *
+ * The exception is that, if you link the Harbour Minigui library with other
+ * files to produce an executable, this does not by itself cause the resulting
+ * executable to be covered by the GNU General Public License.
+ * Your use of that executable is in no way restricted on account of linking the
+ * Harbour-Minigui library code into it.
+ *
+ * Parts of this project are based upon:
+ *
+ * "Harbour GUI framework for Win32"
+ * Copyright 2001 Alexander S.Kresin <alex@kresin.ru>
+ * Copyright 2001 Antonio Linares <alinares@fivetech.com>
+ * www - https://harbour.github.io/
+ *
+ * "Harbour Project"
+ * Copyright 1999-2022, https://harbour.github.io/
+ *
+ * "WHAT32"
+ * Copyright 2002 AJ Wos <andrwos@aust1.net>
+ *
+ * "HWGUI"
+ * Copyright 2001-2021 Alexander S.Kresin <alex@kresin.ru>
+ */
 
 #define _WIN32_IE  0x0501
 
 #include "mgdefs.hpp"
-
 #include <commctrl.h>
+#include <hbwinuni.hpp>
 
-#ifdef UNICODE
-LPWSTR AnsiToWide(LPCSTR);
-#endif
 void pascal DelResource(HANDLE hResource);
-
 
 #ifndef HMG_LEGACY_OFF
 #if !defined( __MINGW32__ ) && ( __HARBOUR__ - 0 > 0x020000 ) && ( __HARBOUR__ - 0 < 0x030200 )
@@ -133,24 +128,23 @@ HB_FUNC( SYSTEMPARAMETERSINFO )
    hb_retl( SystemParametersInfoA(hmg_par_UINT(1), hmg_par_UINT(2), ( VOID * ) hb_parc(3), hmg_par_UINT(4)) );
 }
 
+/*
+GETTEXTWIDTH(HDC, cString, HFONT) --> numeric
+*/
 HB_FUNC( GETTEXTWIDTH ) // returns the width of a string in pixels
 {
-   HDC   hDC        = hmg_par_HDC(1);
-   HWND  hWnd       = ( HWND ) nullptr;
-   BOOL  bDestroyDC = FALSE;
-   HFONT hFont      = hmg_par_HFONT(3);
-   HFONT hOldFont   = ( HFONT ) nullptr;
-   SIZE  sz;
-
-#ifndef UNICODE
-   LPCSTR lpString = hb_parc(2);
-#else
-   LPCWSTR lpString = AnsiToWide(( char * ) hb_parc(2));
-#endif
+   HDC hDC = hmg_par_HDC(1);
+   HWND hWnd = nullptr;
+   bool bDestroyDC = false;
+   HFONT hFont = hmg_par_HFONT(3);
+   HFONT hOldFont = nullptr;
+   SIZE sz;
+   void * String;
+   LPCTSTR lpString = HB_PARSTR(2, &String, nullptr);
 
    if( !hDC )
    {
-      bDestroyDC = TRUE;
+      bDestroyDC = true;
       hWnd       = GetActiveWindow();
       hDC        = GetDC(hWnd);
    }
@@ -174,9 +168,7 @@ HB_FUNC( GETTEXTWIDTH ) // returns the width of a string in pixels
 
    hb_retnl( sz.cx );
 
-#ifdef UNICODE
-   hb_xfree(( TCHAR * ) lpString);
-#endif
+   hb_strfree(String);
 }
 
 HB_FUNC( KEYBD_EVENT )
@@ -283,36 +275,3 @@ HB_FUNC( MOVEBTNTEXTBOX )   //MoveBtnTextBox(hEdit, hBtn1, hBtn2, fBtn2, BtnWidt
       }
    }
 }
-
-#if ( __HARBOUR__ - 0 < 0x030200 )
-
-#include <hbapiitm.hpp>
-#include <hbapicdp.hpp>
-#include <hbapierr.hpp>
-
-HB_FUNC( HB_DATE )
-{
-   hb_retd( hb_parni(1), hb_parni(2), hb_parni(3) );
-}
-
-#if ( __HARBOUR__ - 0 < 0x030200 )
-# define hb_cdppage  hb_vmCDP
-#endif
-
-HB_FUNC( HB_LEFTEQI )
-{
-   PHB_ITEM pItem1 = hb_param(1, Harbour::Item::STRING);
-   PHB_ITEM pItem2 = hb_param(2, Harbour::Item::STRING);
-
-   if( pItem1 && pItem2 )
-   {
-      hb_retl( hb_cdpicmp(hb_itemGetCPtr(pItem1), hb_itemGetCLen(pItem1),
-                          hb_itemGetCPtr(pItem2), hb_itemGetCLen(pItem2), hb_cdppage(), HB_FALSE) == 0 );
-   }
-   else
-   {
-      hb_errRT_BASE_SubstR( EG_ARG, 1071, nullptr, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS );
-   }
-}
-
-#endif
