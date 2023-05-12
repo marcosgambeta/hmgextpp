ad1937f4c4d3854b51085fe0564f1a910b84a57d
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2023-05-12 06:24:34 -0300

    Update WinReport

--- source/WinReport/c_wrepint.cpp
+++ source/WinReport/c_wrepint.cpp
@@ -18,103 +18,114 @@ void CALLBACK MessageBoxTimer(HWND hwnd, UINT uiMsg, UINT idEvent, DWORD dwTime)
    PostQuitMessage(0);
 }
 
-UINT
-TimedMessageBox(
-    HWND hwndParent,
-    LPCTSTR ptszMessage,
-    LPCTSTR ptszTitle,
-    UINT flags,
-    DWORD dwTimeout)
+UINT TimedMessageBox(HWND hwndParent, LPCTSTR ptszMessage, LPCTSTR ptszTitle, UINT flags, DWORD dwTimeout)
 {
-    UINT idTimer;
-    UINT uiResult;
-    MSG msg;
+   /*
+    *  Set a timer to dismiss the Message box.
+    */
+   UINT idTimer = SetTimer(nullptr, 0, dwTimeout, reinterpret_cast<TIMERPROC>(MessageBoxTimer));
 
-    /*
-     *  Set a timer to dismiss the Message box.
-     */
-    idTimer = SetTimer(nullptr, 0, dwTimeout, (TIMERPROC)MessageBoxTimer);
+   UINT uiResult = MessageBox(hwndParent, ptszMessage ? ptszMessage : "", ptszTitle ? ptszTitle : "", flags);
 
-    uiResult = MessageBox(hwndParent, ptszMessage ? ptszMessage : "", ptszTitle ? ptszTitle :"", flags);
+   /*
+    *  Finished with the timer.
+    */
+   KillTimer(nullptr, idTimer);
 
-    /*
-     *  Finished with the timer.
-     */
-    KillTimer(nullptr, idTimer);
+   /*
+    *  See if there is a WM_QUIT Message in the queue. If so,
+    *  then you timed out. Eat the Message so you don't quit the
+    *  entire application.
+    */
+   MSG msg;
 
-    /*
-     *  See if there is a WM_QUIT Message in the queue. If so,
-     *  then you timed out. Eat the Message so you don't quit the
-     *  entire application.
-     */
-    if (PeekMessage(&msg, nullptr, WM_QUIT, WM_QUIT, PM_REMOVE)) {
+   if( PeekMessage(&msg, nullptr, WM_QUIT, WM_QUIT, PM_REMOVE) )
+   {
+       /*
+        *  If you timed out, then return zero.
+        */
+       uiResult = 0;
+   }
 
-        /*
-         *  If you timed out, then return zero.
-         */
-        uiResult = 0;
-    }
-
-    return uiResult;
+   return uiResult;
 }
 
+/*
+C_T_MSGRETRYCANCEL(message, title, timeout) --> numeric
+*/
 HB_FUNC( C_T_MSGRETRYCANCEL )
 {
-        int r;
-        r = TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_RETRYCANCEL | MB_ICONQUESTION | MB_SYSTEMMODAL, hb_parni(3));
-        hb_retni ( r );
+   hb_retni(TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_RETRYCANCEL | MB_ICONQUESTION | MB_SYSTEMMODAL, hb_parni(3)));
 }
 
+/*
+C_T_MSGOKCANCEL(message, title, timeout) --> numeric
+*/
 HB_FUNC( C_T_MSGOKCANCEL )
 {
-        int r;
-        r = TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_OKCANCEL | MB_ICONQUESTION | MB_SYSTEMMODAL, hb_parni(3));
-        hb_retni ( r );
+   hb_retni(TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_OKCANCEL | MB_ICONQUESTION | MB_SYSTEMMODAL, hb_parni(3)));
 }
 
+/*
+C_T_MSGYESNO(message, title, timeout) --> numeric
+*/
 HB_FUNC( C_T_MSGYESNO )
 {
-        int r;
-        r = TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_YESNO | MB_ICONQUESTION | MB_SYSTEMMODAL, hb_parni(3));
-        hb_retni ( r );
-
+   hb_retni(TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_YESNO | MB_ICONQUESTION | MB_SYSTEMMODAL, hb_parni(3)));
 }
 
+/*
+C_T_MSGYESNO_ID(message, title, timeout) --> numeric
+*/
 HB_FUNC( C_T_MSGYESNO_ID )
 {
-        int r;
-        r = TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_YESNO | MB_ICONQUESTION | MB_SYSTEMMODAL | MB_DEFBUTTON2, hb_parni(3));
-        hb_retni ( r );
+   hb_retni(TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_YESNO | MB_ICONQUESTION | MB_SYSTEMMODAL | MB_DEFBUTTON2, hb_parni(3)));
 }
 
+/*
+C_T_MSGBOX(message, title, timeout) --> NIL
+*/
 HB_FUNC( C_T_MSGBOX )
 {
-        TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_SYSTEMMODAL, hb_parni(3));
+   TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_SYSTEMMODAL, hb_parni(3));
 }
 
+/*
+C_T_MSGINFO(message, title, timeout) --> NIL
+*/
 HB_FUNC( C_T_MSGINFO )
 {
-        TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_OK | MB_ICONINFORMATION | MB_SYSTEMMODAL, hb_parni(3));
+   TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_OK | MB_ICONINFORMATION | MB_SYSTEMMODAL, hb_parni(3));
 }
 
+/*
+C_T_MSGSTOP(message, title, timeout) --> NIL
+*/
 HB_FUNC( C_T_MSGSTOP )
 {
-        TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_OK | MB_ICONSTOP | MB_SYSTEMMODAL, hb_parni(3));
+   TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_OK | MB_ICONSTOP | MB_SYSTEMMODAL, hb_parni(3));
 }
 
+/*
+C_T_MSGEXCLAMATION(message, title, timeout) --> NIL
+*/
 HB_FUNC( C_T_MSGEXCLAMATION )
 {
-        TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_ICONEXCLAMATION | MB_OK | MB_SYSTEMMODAL, hb_parni(3));
+   TimedMessageBox(nullptr, hb_parc(1), hb_parc(2), MB_ICONEXCLAMATION | MB_OK | MB_SYSTEMMODAL, hb_parni(3));
 }
 
+/*
+_HMG_PRINTER_GETPRINTABLEAREAPHYSICALWIDTH(HDC) --> numeric
+*/
 HB_FUNC( _HMG_PRINTER_GETPRINTABLEAREAPHYSICALWIDTH )
 {
-        HDC hdc = hmg_par_HDC(1);
-        hb_retnl ( GetDeviceCaps ( hdc , PHYSICALWIDTH ) );
+   hb_retnl(GetDeviceCaps(hmg_par_HDC(1), PHYSICALWIDTH));
 }
 
+/*
+_HMG_PRINTER_GETPRINTABLEAREAPHYSICALHEIGTH(HDC) --> numeric
+*/
 HB_FUNC( _HMG_PRINTER_GETPRINTABLEAREAPHYSICALHEIGTH )
 {
-        HDC hdc = hmg_par_HDC(1);
-        hb_retnl ( GetDeviceCaps ( hdc , PHYSICALHEIGHT ) );
+   hb_retnl(GetDeviceCaps(hmg_par_HDC(1), PHYSICALHEIGHT));
 }
