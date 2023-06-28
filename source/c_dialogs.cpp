0f80b914a74d505ef24851c00ceef825bbcf3bca
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2023-06-28 19:46:43 -0300

    Update c_dialogs.cpp

--- source/c_dialogs.cpp
+++ source/c_dialogs.cpp
@@ -51,62 +51,30 @@
 #include <commdlg.h>
 #include <shlobj.h>
 #include <commctrl.h>
+#include <hbapiitm.hpp>
+#include <hbwinuni.hpp>
 
 #ifdef UNICODE
 LPWSTR AnsiToWide(LPCSTR);
-LPSTR  WideToAnsi(LPWSTR);
 #endif
 
-HB_FUNC( CHOOSEFONT ) {
-   CHOOSEFONT cf;
-   LOGFONT    lf;
-   long       PointSize;
-   int        bold;
-   HDC        hdc;
-   HWND       hwnd;
+HB_FUNC( CHOOSEFONT )
+{
+   HWND hwnd = GetActiveWindow();
+   HDC hdc  = GetDC(hwnd);
 
-#ifdef UNICODE
-   LPSTR  pStr;
-   LPWSTR pWStr = AnsiToWide(hb_parc(1));
-   lstrcpy(lf.lfFaceName, pWStr);
-   hb_xfree(pWStr);
-#else
-   lstrcpy(lf.lfFaceName, hb_parc(1));
-#endif
-
-   hwnd = GetActiveWindow();
-   hdc  = GetDC(hwnd);
-
+   LOGFONT lf{};
+   void * str;
+   lstrcpy(lf.lfFaceName, HB_PARSTR(1, &str, nullptr));
+   hb_strfree(str);
    lf.lfHeight = -MulDiv(hb_parnl(2), GetDeviceCaps(hdc, LOGPIXELSY), 72);
+   lf.lfWeight = hb_parl(3) ? 700 : 400;
+   lf.lfItalic = hb_parl(4) ? TRUE : FALSE;
+   lf.lfUnderline = hb_parl(6) ? TRUE : FALSE;
+   lf.lfStrikeOut = hb_parl(7) ? TRUE : FALSE;
+   lf.lfCharSet = HB_ISNIL(8) ? DEFAULT_CHARSET : hb_parni(8);
 
-   if( hb_parl(3) ) {
-      lf.lfWeight = 700;
-   } else {
-      lf.lfWeight = 400;
-   }
-
-   if( hb_parl(4) ) {
-      lf.lfItalic = TRUE;
-   } else {
-      lf.lfItalic = FALSE;
-   }
-
-   if( hb_parl(6) ) {
-      lf.lfUnderline = TRUE;
-   } else {
-      lf.lfUnderline = FALSE;
-   }
-
-   if( hb_parl(7) ) {
-      lf.lfStrikeOut = TRUE;
-   } else {
-      lf.lfStrikeOut = FALSE;
-   }
-
-   lf.lfCharSet = ( BYTE ) ( HB_ISNIL(8) ? DEFAULT_CHARSET : hb_parni(8) );
-
-   ZeroMemory(&cf, sizeof(cf));
-
+   CHOOSEFONT cf{};
    cf.lStructSize = sizeof(CHOOSEFONT);
    cf.hwndOwner   = hwnd;
    cf.hDC         = nullptr;
@@ -134,36 +102,25 @@ HB_FUNC( CHOOSEFONT ) {
       return;
    }
 
-   PointSize = -MulDiv(lf.lfHeight, 72, GetDeviceCaps(hdc, LOGPIXELSY));
+   long PointSize = -MulDiv(lf.lfHeight, 72, GetDeviceCaps(hdc, LOGPIXELSY));
+   int bold = (lf.lfWeight == 700) ? 1 : 0;
 
-   if( lf.lfWeight == 700 ) {
-      bold = 1;
-   } else {
-      bold = 0;
-   }
-
-   hb_reta(8);
-#ifndef UNICODE
-   HB_STORC( lf.lfFaceName, -1, 1 );
-#else
-   pStr = WideToAnsi(lf.lfFaceName);
-   HB_STORC( pStr, -1, 1 );
-   hb_xfree(pStr);
-#endif
-   HB_STORVNL( PointSize, -1, 2 );
-   HB_STORL( bold, -1, 3 );
-   HB_STORL( lf.lfItalic, -1, 4 );
-   HB_STORVNL( cf.rgbColors, -1, 5 );
-   HB_STORL( lf.lfUnderline, -1, 6 );
-   HB_STORL( lf.lfStrikeOut, -1, 7 );
-   HB_STORNI( lf.lfCharSet, -1, 8 );
+   PHB_ITEM pArray = hb_itemArrayNew(8);
+   HB_ARRAYSETSTR(pArray, 1, lf.lfFaceName);
+   hb_arraySetNL(pArray, 2, PointSize);
+   hb_arraySetL(pArray, 3, bold);
+   hb_arraySetL(pArray, 4, lf.lfItalic);
+   hb_arraySetNL(pArray, 5, cf.rgbColors);
+   hb_arraySetL(pArray, 6, lf.lfUnderline);
+   hb_arraySetL(pArray, 7, lf.lfStrikeOut);
+   hb_arraySetNI(pArray, 8, lf.lfCharSet);
+   hb_itemReturnRelease(pArray);
 
    ReleaseDC(hwnd, hdc);
 }
 
 HB_FUNC( C_GETFILE )
 {
-   OPENFILENAME ofn;
    TCHAR        buffer[32768];
    TCHAR        cFullName[256][1024];
    TCHAR        cCurDir[512];
@@ -208,36 +165,24 @@ HB_FUNC( C_GETFILE )
       iFilterIndex = hb_parni(6);
    }
 
-   memset(( void * ) &ofn, 0, sizeof(OPENFILENAME));
+   OPENFILENAME ofn{};
    ofn.lStructSize = sizeof(ofn);
    ofn.hwndOwner   = GetActiveWindow();
-   #ifndef UNICODE
-   ofn.lpstrFilter     = hb_parc(1);
+   void * str1;
+   ofn.lpstrFilter     = HB_PARSTR(1, &str1, nullptr);
    ofn.lpstrFile       = buffer;
-   ofn.lpstrInitialDir = hb_parc(3);
-   ofn.lpstrTitle      = hb_parc(2);
-   #else
-   pW1 = AnsiToWide(hb_parc(3));
-   pW2 = AnsiToWide(hb_parc(2));
-   ofn.lpstrFilter     = ( LPCTSTR ) &Filter;
-   ofn.lpstrFile       = ( LPTSTR ) &buffer;
-   ofn.lpstrInitialDir = pW1;
-   ofn.lpstrTitle      = pW2;
-   #endif
-   ofn.nFilterIndex  = ( DWORD ) iFilterIndex;
+   void * str2;
+   ofn.lpstrInitialDir = HB_PARSTR(3, &str2, nullptr);
+   void * str3;
+   ofn.lpstrTitle      = HB_PARSTR(2, &str3, nullptr);
+   ofn.nFilterIndex  = iFilterIndex;
    ofn.nMaxFile      = sizeof(buffer) / sizeof(TCHAR);
    ofn.nMaxFileTitle = 512;
    ofn.Flags         = flags;
 
    if( GetOpenFileName(&ofn) ) {
       if( ofn.nFileExtension != 0 ) {
-#ifndef UNICODE
-         hb_retc( ofn.lpstrFile );
-#else
-         pStr = hb_osStrU16Decode(ofn.lpstrFile);
-         hb_retc( pStr );
-         hb_xfree(pStr);
-#endif
+         HB_RETSTR( ofn.lpstrFile );
       } else {
          wsprintf( cCurDir, "%s", &buffer[iPosition] );
          iPosition = iPosition + lstrlen(cCurDir) + 1;
@@ -262,22 +207,16 @@ HB_FUNC( C_GETFILE )
 #endif
             }
          } else {
-#ifndef UNICODE
-            hb_retc( &buffer[0] );
-#else
-            pStr = WideToAnsi(&buffer[0]);
-            hb_retc( pStr );
-            hb_xfree(pStr);
-#endif
+            HB_RETSTR(&buffer[0]);
          }
       }
    } else {
-#ifndef UNICODE
-      hb_retc( "" );
-#else
-      hb_retc( WideToAnsi("") );
-#endif
+      HB_RETSTR("");
    }
+
+   hb_strfree(str1);
+   hb_strfree(str2);
+   hb_strfree(str3);
 
 #ifdef UNICODE
    hb_xfree(pW1);
@@ -287,11 +226,9 @@ HB_FUNC( C_GETFILE )
 
 HB_FUNC( C_PUTFILE ) // JK JP
 {
-   OPENFILENAME ofn;
    TCHAR        buffer[512];
    TCHAR        cExt[4];
    int          iFilterIndex = 1;
-   DWORD        flags        = OFN_FILEMUSTEXIST | OFN_EXPLORER;
 
 #ifdef UNICODE
    LPWSTR pW, pW1, pW2;
@@ -312,6 +249,8 @@ HB_FUNC( C_PUTFILE ) // JK JP
       }
    }
 #endif
+
+   DWORD        flags        = OFN_FILEMUSTEXIST | OFN_EXPLORER;
 
    if( hb_parl(4) ) {
       flags |= OFN_NOCHANGEDIR;
@@ -339,23 +278,17 @@ HB_FUNC( C_PUTFILE ) // JK JP
       iFilterIndex = hb_parni(6);
    }
 
-   memset(( void * ) &ofn, 0, sizeof(OPENFILENAME));
+   OPENFILENAME ofn{};
    ofn.lStructSize = sizeof(ofn);
    ofn.hwndOwner   = GetActiveWindow();
-   #ifndef UNICODE
-   ofn.lpstrFilter     = hb_parc(1);
+   void * str1;
+   ofn.lpstrFilter     = HB_PARSTR(1, &str1, nullptr);
    ofn.lpstrFile       = buffer;
-   ofn.lpstrInitialDir = hb_parc(3);
-   ofn.lpstrTitle      = hb_parc(2);
-   #else
-   pW1 = AnsiToWide(hb_parc(3));
-   pW2 = AnsiToWide(hb_parc(2));
-   ofn.lpstrFilter     = ( LPCTSTR ) &Filter;
-   ofn.lpstrFile       = ( LPTSTR ) &buffer;
-   ofn.lpstrInitialDir = pW1;
-   ofn.lpstrTitle      = pW2;
-   #endif
-   ofn.nFilterIndex = ( DWORD ) iFilterIndex;
+   void * str2;
+   ofn.lpstrInitialDir = HB_PARSTR(3, &str2, nullptr);
+   void * str3;
+   ofn.lpstrTitle      = HB_PARSTR(2, &str3, nullptr);
+   ofn.nFilterIndex = iFilterIndex;
    ofn.nMaxFile     = sizeof(buffer) / sizeof(TCHAR);
    ofn.Flags        = flags;
    ofn.lpstrDefExt  = cExt;
@@ -369,20 +302,14 @@ HB_FUNC( C_PUTFILE ) // JK JP
          hb_storni( ofn.nFilterIndex, 6 );
       }
 
-#ifndef UNICODE
-      hb_retc( ofn.lpstrFile );
-#else
-      pStr = hb_osStrU16Decode(ofn.lpstrFile);
-      hb_retc( pStr );
-      hb_xfree(pStr);
-#endif
+      HB_RETSTR(ofn.lpstrFile);
    } else {
-#ifndef UNICODE
-      hb_retc( "" );
-#else
-      hb_retc( WideToAnsi("") );
-#endif
+      HB_RETSTR("");
    }
+
+   hb_strfree(str1);
+   hb_strfree(str2);
+   hb_strfree(str3);
 
 #ifdef UNICODE
    hb_xfree(pW1);
@@ -403,11 +330,7 @@ int CALLBACK BrowseCallbackProc(HWND hWnd, UINT uMsg, LPARAM lParam, LPARAM lpDa
       case BFFM_INITIALIZED:
          if( lpData ) {
             SendMessage(hWnd, BFFM_SETSELECTION, TRUE, lpData);
-#ifndef UNICODE
-            SetWindowText(hWnd, ( LPCSTR ) s_szWinName);
-#else
-            SetWindowText(hWnd, ( LPCWSTR ) s_szWinName);
-#endif
+            SetWindowText(hWnd, ( LPCTSTR ) s_szWinName);
          }
          break;
       case BFFM_VALIDATEFAILED:
@@ -423,80 +346,60 @@ int CALLBACK BrowseCallbackProc(HWND hWnd, UINT uMsg, LPARAM lParam, LPARAM lpDa
    return 0;
 }
 
+/*
+C_BROWSEFORFOLDER(HWND, cTitle, nFlags, np4, cp5) --> string
+*/
 HB_FUNC( C_BROWSEFORFOLDER )  // Syntax: C_BROWSEFORFOLDER([<hWnd>],[<cTitle>],[<nFlags>],[<nFolderType>],[<cInitPath>])
 {
-   HWND         hWnd = HB_ISNIL(1) ? GetActiveWindow() : hmg_par_HWND(1);
-   BROWSEINFO   BrowseInfo;
-   TCHAR        lpBuffer[MAX_PATH];
-   LPITEMIDLIST pidlBrowse;
-
-#ifdef UNICODE
-   LPWSTR pW, pW2;
-   LPSTR  pStr;
-#endif
+   HWND hWnd = HB_ISNIL(1) ? GetActiveWindow() : hmg_par_HWND(1);
 
    if( HB_ISCHAR(5) ) {
-#ifndef UNICODE
-      GetWindowText(hWnd, ( LPSTR ) s_szWinName, MAX_PATH);
-#else
-      GetWindowText(hWnd, ( LPWSTR ) s_szWinName, MAX_PATH);
-#endif
+      GetWindowText(hWnd, ( LPTSTR ) s_szWinName, MAX_PATH);
    }
 
+   LPITEMIDLIST pidlBrowse;
    SHGetSpecialFolderLocation(hWnd, HB_ISNIL(4) ? CSIDL_DRIVES : hb_parni(4), &pidlBrowse);
 
+   TCHAR lpBuffer[MAX_PATH];
+
+   BROWSEINFO BrowseInfo{};
    BrowseInfo.hwndOwner      = hWnd;
    BrowseInfo.pidlRoot       = pidlBrowse;
    BrowseInfo.pszDisplayName = lpBuffer;
-#ifndef UNICODE
-   BrowseInfo.lpszTitle = HB_ISNIL(2) ? "Select a Folder" : hb_parc(2);
-#else
-   pW = AnsiToWide(hb_parc(2));
-   BrowseInfo.lpszTitle = HB_ISNIL(2) ? "Select a Folder" : pW;
-#endif
-   BrowseInfo.ulFlags = hb_parni(3) | ( HB_ISCHAR(5) ? BIF_STATUSTEXT | BIF_RETURNONLYFSDIRS : 0 );
-   BrowseInfo.lpfn    = BrowseCallbackProc;
-#ifndef UNICODE
-   BrowseInfo.lParam = HB_ISCHAR(5) ? reinterpret_cast<LPARAM>(hb_parc(5)) : 0;
-#else
-   pW2 = AnsiToWide(hb_parc(5));
-   BrowseInfo.lParam = HB_ISCHAR(5) ? reinterpret_cast<LPARAM>(pW2) : 0;
-#endif
-   BrowseInfo.iImage = 0;
+   void * str1 = nullptr;
+   BrowseInfo.lpszTitle      = HB_ISNIL(2) ? "Select a Folder" : HB_PARSTR(2, &str1, nullptr);
+   BrowseInfo.ulFlags        = hb_parni(3) | ( HB_ISCHAR(5) ? BIF_STATUSTEXT | BIF_RETURNONLYFSDIRS : 0 );
+   BrowseInfo.lpfn           = BrowseCallbackProc;
+   void * str2 = nullptr;
+   BrowseInfo.lParam         = HB_ISCHAR(5) ? reinterpret_cast<LPARAM>(HB_PARSTR(5, &str2, nullptr)) : 0;
+   BrowseInfo.iImage         = 0;
 
    pidlBrowse = SHBrowseForFolder( &BrowseInfo );
 
    if( pidlBrowse ) {
       SHGetPathFromIDList(pidlBrowse, lpBuffer);
-#ifndef UNICODE
-      hb_retc( lpBuffer );
-#else
-      pStr = hb_osStrU16Decode(lpBuffer);
-      hb_retc( pStr );
-      hb_xfree(pStr);
-#endif
+      HB_RETSTR(lpBuffer);
    } else {
-      hb_retc( "" );
+      hb_retc("");
    }
 
    CoTaskMemFree(pidlBrowse);
-   #ifdef UNICODE
-   hb_xfree(pW);
-   hb_xfree(pW2);
-   #endif
+
+   hb_strfree(str1);
+   hb_strfree(str2);
 }
 
+/*
+CHOOSECOLOR(HWND, nColor, ap3, np4) --> nColor|-1
+*/
 HB_FUNC( CHOOSECOLOR )
 {
-   CHOOSECOLOR cc;
-   COLORREF    crCustClr[16];
-
+   COLORREF crCustClr[16];
    for( int i = 0; i < 16; i++ ) {
       crCustClr[i] = ( HB_ISARRAY(3) ? ( COLORREF ) HB_PARVNL(3, i + 1) : GetSysColor(COLOR_BTNFACE) );
    }
 
-   memset(&cc, 0, sizeof(cc));
-
+   CHOOSECOLOR cc{};
    cc.lStructSize  = sizeof(CHOOSECOLOR);
    cc.hwndOwner    = HB_ISNIL(1) ? GetActiveWindow() : hmg_par_HWND(1);
    cc.rgbResult    = ( COLORREF ) HB_ISNIL(2) ? 0 : hb_parnl(2);
@@ -510,22 +413,18 @@ HB_FUNC( CHOOSECOLOR )
    }
 }
 
+/*
+UNITSTOPIXELSX(nUnitsX) --> cx
+*/
 HB_FUNC( UNITSTOPIXELSX )
 {
-   int   UnitsX = hb_parni(1);
-   DWORD dwDLU  = GetDialogBaseUnits();
-
-   int cx = MulDiv(UnitsX, LOWORD(dwDLU), 4);
-
-   hb_retni( cx );
+   hb_retni(MulDiv(hb_parni(1), LOWORD(GetDialogBaseUnits()), 4));
 }
 
+/*
+UNITSTOPIXELSY(nUnitsY) --> cy
+*/
 HB_FUNC( UNITSTOPIXELSY )
 {
-   int   UnitsY = hb_parni(1);
-   DWORD dwDLU  = GetDialogBaseUnits();
-
-   int cy = MulDiv(UnitsY, HIWORD(dwDLU), 8);
-
-   hb_retni( cy );
+   hb_retni(MulDiv(hb_parni(1), HIWORD(GetDialogBaseUnits()), 8));
 }
