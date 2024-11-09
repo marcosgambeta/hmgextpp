262c539aee25fe8fa444c84cb8a0ef569be49e47
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2024-11-09 19:48:54 -0300

    Update mgdefs.hpp add macro hmg_vmPushHWND

--- include/mgdefs.hpp
+++ include/mgdefs.hpp
@@ -247,8 +247,10 @@ HBITMAP HMG_LoadImage(const char * pszImageName, const char * pszTypeOfRes);
 
 #ifdef HMG_USE_POINTERS
 #define hmg_vmPushHandle(x)      hb_vmPushPointer(x)
+#define hmg_vmPushHWND(x)        hb_vmPushPointer(x)
 #else
 #define hmg_vmPushHandle(x)      hb_vmPushLong(reinterpret_cast<LONG_PTR>(x))
+#define hmg_vmPushHWND(x)        hb_vmPushLong(reinterpret_cast<LONG_PTR>(x))
 #endif
 
 #ifdef HMG_USE_POINTERS
